0f631173c32fd871a8bc2ae0ea39950e9fd60c86
Valeria <valzilla@ymail.com>
2018-06-03 23:45:57 -0300

    first

---
+++ README.md
@@ -0,0 +1,1 @@
+# valzilla.github.io
---
+++ js/index.js
@@ -0,0 +1,21 @@
+$(window).scroll(function(){
+  var scroll = $(document).scrollTop();
+  var navH = $(".head").height();
+
+  if (scroll > navH) {
+    $(".head").addClass("show");
+
+  } else {
+    $(".head").removeClass("show");
+  }
+})
+
+var lang = navigator.language || navigator.userLanguage;
+
+if (lang.indexOf("es") > -1 ) {
+  //$(".nom h3").text("Front End Web Development");
+  //$("#skill h2").text("SKILLS");
+  $(".spa").css("display","inline-block");
+} else {
+  $(".eng").css("display","inline-block");
+}
